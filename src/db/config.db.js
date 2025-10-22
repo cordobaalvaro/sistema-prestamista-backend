97f0148313625af9ced6e8db6469af4e96b9deb5
Alvaro Cordoba <cordobaalvaromaximiliano@gmail.com>
2025-10-22 15:07:32 -0300

    DB: quitar timeouts de Mongoose

--- src/db/config.db.js
+++ src/db/config.db.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Evitar que Mongoose falle por "buffering timed out" (0 = sin timeout)
-mongoose.set("bufferTimeoutMS", 0);
-// Opcional: compatibilidad de queries
+mongoose.set("bufferTimeoutMS", 2147483647);
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(process.env.MONGO_ACCESS, {
-    serverSelectionTimeoutMS: 60000, // esperar hasta 60s para seleccionar servidor
-    socketTimeoutMS: 60000, // tiempo de espera de sockets
-    // maxPoolSize: 10, // opcional, tamaño del pool
+    serverSelectionTimeoutMS: 86400000,
+    connectTimeoutMS: 86400000,
+    socketTimeoutMS: 0,
   })
   .then(() => console.log("desde la base de datos"))
   .catch((error) => console.log("Error conectando a DB:", error));
